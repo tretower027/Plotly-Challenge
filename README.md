f2a97ba99f5200c360c70ba4160361137d5728de
tretower027 <65380381+tretower027@users.noreply.github.com>
2020-10-02 18:36:00 -0700

    final commit

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # Plotly-Challenge
+
+In this challenge I am building an interactive dashboard to explore the Belly Button Biodiversity dataset, which catalogs the microbes that colonize human navels. The dataset reveals that a small handful of microbial species (also called operational taxonomic units, or OTUs, in the study) were present in more than 70% of people, while the rest were relatively rare.
+
+I achieved this by using Plotly to create visualizations such as a bar chart, a bubble chart, and demographic info section that displays an individual’s demographic information. Each chart works directly with demographic information section and dropdown that will let someone choose an induvial person’s ID.
+
+The horizontal bar chart displays the top 10 OTU’s found per person. I used the sample_values as the values for the chart and the OTU_ids as the labels.
+
+The bubble chart displays each sample using OTU_ids as the x-values and the sample_values as the y-values. I then used the OTU_ids for the colors and the sample_values for the marker size. 
---
+++ StarterCode/static/js/app.js
@@ -0,0 +1,98 @@
+function pageLoader(){
+    var selecter = d3.select("#selDataset")
+    
+    
+    d3.json("samples.json").then(function(data) {
+        // console.log(data);
+
+        data.names.forEach((name) => {
+            selecter.append("option").text(name).property("value", name)
+        })
+        
+        createTable(data.names[0])
+        createChart(data.names[0])
+      });
+   
+}
+function optionChanged(newName){
+    createTable(newName)
+    createChart(newName)
+}
+
+function createTable(sampleName){
+    var panel = d3.select("#sample-metadata")
+    panel.html("")
+    d3.json("samples.json").then(function(data) {
+        var filterNames = data.metadata.filter(data => data.id == sampleName)[0]
+        Object.entries(filterNames).forEach(([key, value]) => {
+            panel.append("h5").text(`${key}: ${value}`)
+        })
+
+        // console.log(filterNames);
+    })
+}
+
+function createChart(charts){
+    d3.json("samples.json").then(function(data){
+        var filterSamples = data.samples.filter(data => data.id == charts)[0]
+        console.log(filterSamples)
+        var otuIds = filterSamples.otu_ids
+        var otuLabels = filterSamples.otu_labels
+        var sampleValues = filterSamples.sample_values
+
+        //create bar chart trace
+        var barTrace = {
+            x: sampleValues.slice(0,10).reverse(),
+            y: otuIds.slice(0,10).map(otuIds => `OTU ${otuIds}`).reverse(),
+            mode: "markers",
+            marker: {size:25},
+            text: otuLabels.slice(0,10).reverse(),
+            type: "bar",
+            orientation: 'h',
+        };
+
+        var barData = [barTrace];
+          
+        var barLayout = {
+            title: "<b>Top 10 OTUs</b>",
+            margin: { t: 100, b: 50, l:30, r:25 } 
+          };
+          
+        Plotly.newPlot("bar", barData, barLayout);
+
+        //create bubble chart trace
+        var bubbleTrace = {
+            x: otuIds,
+            y: sampleValues,
+            mode: "markers",
+            marker: {
+                size: sampleValues,
+                color: otuIds,
+                colorscale: "continent"
+            },
+            text: otuLabels
+
+        }
+
+        var bubbleData = [bubbleTrace]
+
+        var bubbleLayout = {
+            xaxis: {title: "OTU ID"},
+            height: 600,
+            width: 1200
+
+        }
+
+        Plotly.newPlot("bubble", bubbleData, bubbleLayout);
+
+
+        
+        
+    })
+
+}
+
+
+  
+
+pageLoader()
